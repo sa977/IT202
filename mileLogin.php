1dd7d2b3b3359d75e7766e226903d5f5274c78c7
sa977 <sa977@njit.edu>
2019-11-01 18:12:18 -0400

    mileLogin

--- mileLogin.php
+++ mileLogin.php
@@ -1,12 +1,61 @@
 <!DOCTYPE html>  
 <?php
+
+
+function get_sample_users(){
+	require('config.php');
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	try{
+		$db = new PDO($conn_string, $username, $password);
+		$select_query = "select username, pin from `TestUsers`";
+		$stmt = $db->prepare($select_query);
+		$r = $stmt->execute();
+		$response = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+	}
+	catch(Exception $e){
+		$response = "DB Error: " . $e;
+	}
+	return $response;
+}
+if(isset($_POST["type"])){
+	$type = $_POST["type"];
+	$response = "nothing";
+	switch($type){
+		case "db":
+			$response = get_sample_users();
+			break;
+	}
+	echo $response;
+}
+
+
+//function onSubmit(){
+//	let form = document.forms[0];
+//	let sv = form.type.value;
+//	switch(sv){
+//		case "db": sampleDB();
+//			break;
+//	}
+//
+//function sampleDB(){
+//	let cont = document.getElementById("output");
+//	myAjax("POST", "db", cont);
+//}
+
+
+
+
+
+
+
+
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 function checkPasswords(){
 	if(isset($_POST['password']) && isset($_POST['confirm'])){
 		if($_POST['password'] == $_POST['confirm']){
-			echo "<br>Passwords Matched!<br>";
+			echo "<br>Welcome Back!<br>";
 		}
 		else{
 			echo "<br>Passwords didn't match!<br>";
@@ -25,11 +74,13 @@ function validate(){
 	console.log(conf);
 	let pv = document.getElementById("validation.password");
 	let succeeded = true;
+
 	if(password == conf){
 		
 		pv.style.display = "none";
 		form.confirm.className= "noerror";	
 	}
+
 	else{
 		pv.style.display = "block";
 		pv.innerText = "Passwords don't match";
@@ -38,36 +89,39 @@ function validate(){
 		//form.confirm.style = "border: 1px solid red;";
 		succeeded = false;
 	}
-	var email = form.email.value;
-	var ev = document.getElementById("validation.email");
-	if(email.indexOf('@') > -1){
-		ev.style.display = "none";
-	}
-	else{
-		ev.style.display = "block";
-		ev.innerText = "Please enter a valid email address";
-		succeeded = false;
-	}
+
+//	var email = form.email.value;
+//	var ev = document.getElementById("validation.email");
+//	if(email.indexOf('@') > -1){
+//		ev.style.display = "none";
+//	}
+//
+//	else{
+//		ev.style.display = "block";
+//		ev.innerText = "Please enter a valid email address";
+//		succeeded = false;
+//	}
 
 	return succeeded;	
 }
+
 </script>
 <style>
 input { border: 1px solid black; }
 .error {border: 1px solid red;}
 .noerror {border: 1px solid black;}
 </style>
+
 </head>
 <body>
 <div style="margin-left: 50%; margin-right:50%;">
 <form method="POST" action="#" onsubmit="return validate();">
-<input name="name" type="text" placeholder="Enter your name"/>
 
-<input name="email" type="email" placeholder="name@example.com"/>
-<span id="validation.email" style="display:none;"></span>
+<input name="Username" type="name" placeholder="Username"/>
+<span id="validation.name" style="display:none;"></span>
 
-<input type="password" name="password" placeholder="Enter password"/>
-<input type="password" name="confirm" placeholder="Re-Enter password"/>
+<input type="password" name="password" placeholder="Enter Password"/>
+<input type="password" name="confirm" placeholder="Re-Enter Password"/>
 <span style="display:none;" id="validation.password"></span>
 
 <input type="submit" value="Log In"/>
@@ -75,7 +129,9 @@ input { border: 1px solid black; }
 </div>
 </body>
 </html>
+
 <?php checkPasswords();?>
+
 <?php
 if(isset($_POST)){
 	echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
